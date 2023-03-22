447853f41934af2ce1421533724b1f54ed80af87
Vinicius de Pinho Santos (CIEE) <vpinhosantos@ipt.br>
2023-03-22 09:56:32 -0300

    Começando projeto, fazendo a requisição e criando modelo

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RestSharp;
+using System.Text.Json;
+using Tamagotchi.models;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL = "https://pokeapi.co/api/v2/pokemon/1";
+
+            var client = new RestClient(API_URL);
+            RestRequest request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var mascote = JsonSerializer.Deserialize<Mascote>(response.Content);
+                Console.WriteLine(mascote);
+            }
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+            Console.ReadKey();
+
+        }
+    }
+    
+}
